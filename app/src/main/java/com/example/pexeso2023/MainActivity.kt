c4dc11e84c5be3be7d9dd59f0b0309c68f9a5b9a
Zuzanka <hellingebleidd@gmail.com>
2023-06-12 15:44:11 +0200

    inicializuj komponenty

--- app/src/main/java/com/example/pexeso2023/MainActivity.kt
+++ app/src/main/java/com/example/pexeso2023/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.pexeso2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnEasy: Button
+    private lateinit var btnMedium: Button
+    private lateinit var btnHard: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnEasy=findViewById(R.id.buttonEasy)
+        btnMedium=findViewById(R.id.buttonMedium)
+        btnHard=findViewById(R.id.buttonHard)
     }
 }
