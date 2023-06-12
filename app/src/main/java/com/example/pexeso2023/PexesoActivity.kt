ff14501024c31e661de81f72fa85a90dcb541be6
Zuzanka <hellingebleidd@gmail.com>
2023-06-12 15:37:40 +0200

    layouty

---
+++ app/src/main/java/com/example/pexeso2023/PexesoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pexeso2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PexesoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pexeso)
+    }
+}
