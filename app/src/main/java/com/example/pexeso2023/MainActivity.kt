f6987b737da7f8b50835da1873256b433da6b48d
Zuzanka <hellingebleidd@gmail.com>
2023-06-18 17:33:55 +0200

    pridane Logy

--- app/src/main/java/com/example/pexeso2023/MainActivity.kt
+++ app/src/main/java/com/example/pexeso2023/MainActivity.kt
@@ -6,7 +6,12 @@ import android.widget.Button
 import android.content.Intent
 import android.util.Log
 
+
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val TAG = "Intro Screen"
+    }
 
     private lateinit var btnEasy: Button
     private lateinit var btnMedium: Button
@@ -24,16 +29,16 @@ class MainActivity : AppCompatActivity() {
 
         btnEasy.setOnClickListener {
             intent.putExtra("difficulty", EASY_GAME_CARDS)  //ked chcem poslat nejake parametre napr obtiaznost
-            Log.i("VYBER HRY", "zvolena Easy obtiaznost, karty: $EASY_GAME_CARDS")
+            Log.i(TAG, "zvolena Easy obtiaznost, karty: $EASY_GAME_CARDS")
             startActivity(intent)
         }
         btnMedium.setOnClickListener {
-            Log.i("VYBER HRY", "zvolena Medium obtiaznost")
+            Log.i(TAG, "zvolena Medium obtiaznost")
             intent.putExtra("difficulty", MEDIUM_GAME_CARDS)
             startActivity(intent)
         }
         btnHard.setOnClickListener {
-            Log.i("VYBER HRY", "zvolena Hard obtiaznost")
+            Log.i(TAG, "zvolena Hard obtiaznost")
             intent.putExtra("difficulty", HARD_GAME_CARDS)
             startActivity(intent)
         }
--- app/src/main/java/com/example/pexeso2023/PexesoAdapter.kt
+++ app/src/main/java/com/example/pexeso2023/PexesoAdapter.kt
@@ -1,19 +1,30 @@
 package com.example.pexeso2023
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.ImageButton
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import kotlin.math.min
 
 class PexesoAdapter(private val context: Context, private val pocetKariet: Int): RecyclerView.Adapter<PexesoAdapter.KartaViewHolder>() {
 
+    companion object{
+        const val TAG = "Pexeso Adapter"
+    }
+
     class KartaViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+        val kartaButton: ImageButton = itemView.findViewById(R.id.kartaButton)
         fun bind(position: Int) {
             // todo
+            kartaButton.setOnClickListener{
+                Log.i(TAG, "poloha karty: $position")
+            }
+
         }
 
     }
