9169df00f44c509d51429bb5a68e420aa90fcb19
Zuzanka <hellingebleidd@gmail.com>
2023-06-25 03:29:06 +0200

    pripravena databaza

--- app/src/main/java/com/example/pexeso2023/LeaderBoardActivity.kt
+++ app/src/main/java/com/example/pexeso2023/LeaderBoardActivity.kt
@@ -2,10 +2,26 @@ package com.example.pexeso2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pexeso2023.databaza.ScoreViewModel
+import androidx.lifecycle.Observer
 
 class LeaderBoardActivity : AppCompatActivity() {
+    private lateinit var mScoreViewModel: ScoreViewModel
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.leaderboard)
+
+        val adapter=ScoreAdapter()
+        recyclerView = findViewById(R.id.RV_score)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        mScoreViewModel = ViewModelProvider(this).get(ScoreViewModel::class.java)
+        mScoreViewModel.getAll.observe(this, Observer { score -> adapter.setData(score) })
+
     }
 }
---
+++ app/src/main/java/com/example/pexeso2023/databaza/ScoreViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.pexeso2023.databaza
+
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ScoreViewModel(application: Application): AndroidViewModel(application) {
+//class ScoreViewModel(val repo: ScoreRepo): ViewModel() {
+    //provide data to the UI
+
+    val getAll: LiveData<List<Score>> //aj toto by mohlo byt flow
+    private val repo: ScoreRepo
+
+    init{
+        val scoreDao = ScoreDatabase.getDatabase(application).scoreDao()
+        repo = ScoreRepo(scoreDao)
+        getAll = repo.getAll
+
+    }
+
+    fun upsertScore(score: Score){
+        viewModelScope.launch(Dispatchers.IO){
+            repo.upsertScore(score)
+        }
+    }
+//    val score = repo.getAll
+//
+//    class ScoreViewModelFactory(private val repo: ScoreRepo):ViewModelProvider.Factory{
+//        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//            if(modelClass.isAssignableFrom(ScoreViewModel::class.java)){
+//                return ScoreViewModel(repo) as T
+//            }
+//            throw IllegalArgumentException("unknown viewModel class")
+//        }
+//    }
+
+}
