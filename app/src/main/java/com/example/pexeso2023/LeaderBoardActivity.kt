79c030227aa0dc531fe17e8ebecef3458f1023d0
Zuzanka <hellingebleidd@gmail.com>
2023-06-26 18:36:54 +0200

    leaderboard sa po otoceni nezmeni

--- app/src/main/java/com/example/pexeso2023/LeaderBoardActivity.kt
+++ app/src/main/java/com/example/pexeso2023/LeaderBoardActivity.kt
@@ -9,15 +9,17 @@ import androidx.lifecycle.lifecycleScope
 import com.example.pexeso2023.databaza.Score
 import com.example.pexeso2023.databaza.ScoreDatabase
 import com.example.pexeso2023.databinding.LeaderboardBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.launch
 
-
+const val BUNDLE_KEY = "nacitane"
 class LeaderBoardActivity : AppCompatActivity() {
 
     private lateinit var binding: LeaderboardBinding
     private lateinit var buttonTime: Button
     private lateinit var buttonDate: Button
-    var scoreList= emptyList<Score>()
+    private lateinit var scoreList: List<Score>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,7 +27,19 @@ class LeaderBoardActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        nacitajData("")
+        if(savedInstanceState == null){
+            nacitajData("")
+        }else{
+            val jsonScoreList: String? = savedInstanceState.getString(BUNDLE_KEY)
+            val gson=Gson()
+            val listType = object: TypeToken<List<Score>>() {}.type
+            scoreList = gson.fromJson(jsonScoreList,listType)
+            binding.RVScore.apply {
+                layoutManager=LinearLayoutManager(this@LeaderBoardActivity)
+                adapter = ScoreAdapter().apply { setData(scoreList) }
+            }
+        }
+
         buttonTime=findViewById(R.id.order_by_time)
         buttonDate=findViewById(R.id.order_by_date)
 
@@ -43,6 +57,15 @@ class LeaderBoardActivity : AppCompatActivity() {
 
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val savedList = scoreList
+        val gson = Gson() //toto mi urobi Parcelable z listu aby som to vedela ulozit
+        val jsonScoreList:String = gson.toJson(savedList)
+        outState.putString(BUNDLE_KEY,jsonScoreList)
+    }
+
+
     fun nacitajData(parameter: String){
         lifecycleScope.launch {
             when(parameter){
@@ -59,14 +82,12 @@ class LeaderBoardActivity : AppCompatActivity() {
         }
     }
 
-    fun orderByTime(list: List<Score>):List<Score>{
-        val sortedList = list.sortedBy { it.cas }
-        return sortedList
+    fun orderByTime(list: List<Score>): List<Score> {
+        return list.sortedBy { it.cas }
     }
 
-    fun orderByDate(list: List<Score>): List<Score>{
-        val sortedList = list.sortedBy { it.datum }
-        return sortedList
+    fun orderByDate(list: List<Score>): List<Score> {
+        return list.sortedBy { it.datum }
     }
 
 }
