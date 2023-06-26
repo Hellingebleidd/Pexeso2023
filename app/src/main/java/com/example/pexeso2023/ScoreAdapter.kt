729e9af96ae18068487bd1d7a904929fc1f7be39
Zuzanka <hellingebleidd@gmail.com>
2023-06-26 17:17:14 +0200

    tabulka sa naplna, zobrazuje vsetky zaznamy, upraveny layout

--- app/src/main/java/com/example/pexeso2023/ScoreAdapter.kt
+++ app/src/main/java/com/example/pexeso2023/ScoreAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.pexeso2023
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,31 +22,33 @@ class ScoreAdapter: RecyclerView.Adapter<ScoreAdapter.ScoreViewHolder>() {
         val tv_obtiaznost: TextView = itemView.findViewById(R.id.tv_obtiaznost)
         val tv_cas: TextView = itemView.findViewById(R.id.tv_cas)
 
-//        fun bind(score: Score) {
-//            tv_datum.text=score.datum
-//            tv_obtiaznost.text=score.obtiaznost
-//            tv_cas.text=score.cas
-//        }
+        fun bind(score: Score) {
+            tv_datum.text=score.datum
+            tv_obtiaznost.text=score.obtiaznost
+            tv_cas.text=score.cas
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScoreViewHolder {
-        return ScoreViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.score_riadok, parent, false))
-//        return LayoutInflater.from(parent.context).inflate(R.layout.score_riadok,parent,false).let { ScoreViewHolder(it) }
+//        return ScoreViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.score_riadok, parent, false))
+        return LayoutInflater.from(parent.context)
+            .inflate(R.layout.score_riadok_lepsi,parent,false)
+            .let { ScoreViewHolder(it) }
     }
 
     override fun getItemCount() = zoznamScore.size
 
     override fun onBindViewHolder(holder: ScoreViewHolder, position: Int) {
         val score = zoznamScore[position]
-        holder.tv_datum.text = score.datum
-        holder.tv_cas.text=score.cas
-        holder.tv_obtiaznost.text = score.obtiaznost
-//        holder.bind(zoznamScore[position])
+//        holder.tv_datum.text = score.datum
+//        holder.tv_cas.text=score.cas
+//        holder.tv_obtiaznost.text = score.obtiaznost
+        holder.bind(zoznamScore[position])
     }
 
     fun setData(score: List<Score>){
         zoznamScore.apply {
-//            clear()
+            clear()
             addAll(score)
 
         }
